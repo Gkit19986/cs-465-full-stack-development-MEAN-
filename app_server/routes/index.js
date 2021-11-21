8ee932d273d81237a5cd3d6de691c517cf64b698
gkit19986 <michael.farag.abdou86@gmail.com>
2021-11-21 17:46:17 -0500

    add seegoose

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -4,16 +4,3 @@ var router = express.Router();
 const controller = require ('../controllers/main')
 router.get('/',controller.index)
 module.exports = router;
-
-//const ctrlMain = require('../controllers/main');
-/* GET home page. */
-//router.get('/', ctrlMain.index);
-
-/* GET home page. */
-//router.get('/', function(req, res, next) {
-  //res.render('index', { title: 'Express' });
-//});
-
-
-
-//module.exports = router;
